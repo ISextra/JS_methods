3c19db3c3bdedfce29ec4f306a5f5b34faf647e6
ISextra <ivan2001solovei@mail.ru>
2022-08-04 19:07:42 +0300

    stage-1 func1,func2.func3,func4

--- src/js/stage-1.js
+++ src/js/stage-1.js
@@ -5,7 +5,7 @@ function func1 (num) {
     for (let i = num-1;i>1;i--) {
         num *= i;
     }
-    console.log(num);
+    return(num);
 }
 
 //Написать функцию, которая на вход принимает строку и возвращает длину самого
@@ -23,7 +23,7 @@ function func2 (str) {
             length=0;
         }
     }
-    console.log(maxlength);
+    return(maxlength);
 }
 
 //Написать функцию, которая на вход принимает массив, состоящий из массивов целых
@@ -40,10 +40,21 @@ function func3 (arr) {
         }
         arr1.push(maxnum);
     }
-    console.log(arr1);
+    return(arr1);
 }
 
+//Написать функцию, которая на вход принимает строку и целое число и обрезает строку
+// (первый аргумент), если она длиннее заданной максимальной длины строки (второй
+// аргумент) и возвращает обрезанную строку с «…» в конце.
+function func4 (text, length) {
+    for (let i = length; i < text.length-1;i++) {
+        text[i] = "";
+    }
+    text += "...";
+    return(text);
+}//map, splice
 
-//func1(4);
-//func2("hi world");
-func3([[1, 2, 3], [4, 5], [6, 7, 8, 9]]);
+//console.log(func1(4));
+//console.log(func2("hi world"));
+//console.log(func3([[1, 2, 3], [4, 5], [6, 7, 8, 9]]));
+console.log(func4("wasdwasdwasd", 4));
