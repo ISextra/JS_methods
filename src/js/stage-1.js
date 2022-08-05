0901482dc3857fa97440fc10163419dd78f85df8
ISextra <ivan2001solovei@mail.ru>
2022-08-05 17:50:54 +0300

    stage-1 func3 map foreach

--- src/js/stage-1.js
+++ src/js/stage-1.js
@@ -25,17 +25,15 @@ function func2 (str) {
 // чисел (например: [[1, 2, 3], [4, 5], [6, 7, 8, 9]]) и возвращает массив состоящий из
 // наибольшего числа каждого предоставленного подмассива.
 function func3 (arr) {
-    let arr1 = [];
-    for (let i of arr) {
-        let maxnum = i[0];
-        for (let j of i){
-            if (maxnum < j) {
-                maxnum = j;
+    return arr.map(function callback(value,index,arr) {
+        let maxvalue = arr[index][0];
+        value.forEach(function callback(val,inx,value) {
+            if (val > maxvalue) {
+                maxvalue = val;
             }
-        }
-        arr1.push(maxnum);
-    }
-    return(arr1);
+        })
+        return(maxvalue);
+    })
 }
 
 //Написать функцию, которая на вход принимает строку и целое число и обрезает строку
@@ -63,8 +61,22 @@ function func5 (text) {
     return(text);
 }
 
+// Написать функцию, которая на вход принимает два массива и целое число n. Функция
+// должна скопировать каждый элемент первого массива во второй по порядку начиная с
+// индекса n второго массива и вернуть полученный массив. Входные массивы не должны
+// изменяться. (например: при входных данных ([1, 2, 3], [4, 5], 1) должно вернуться
+// [4, 1, 2, 3, 5].
+function func6 (arr1,arr2,num) {
+    let arr11 = arr1.slice();
+    let arr22 = arr2.slice();
+    for (let num of arr11)
+    arr22.splice(num,0,);
+    return(arr22);
+}
+
 //console.log(func1(4));
 //console.log(func2("hi world"));
-//console.log(func3([[1, 2, 3], [4, 5], [6, 7, 8, 9]]));
+console.log(func3([[1, 2, 3], [4, 5], [6, 7, 8, 9]]));
 //console.log(func4("wasdwasdwasd", 4));
 //console.log(func5("GOOD MORNING XFX"));
+//console.log(func6([1, 2, 3], [4, 5], 1));
