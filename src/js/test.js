f4d39af0fe2a08f006ee6216895d32cb64037600
ISextra <ivan2001solovei@mail.ru>
2022-08-12 17:21:25 +0300

    add tests for map2 filter2 find2 some2 every2

--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,5 @@
 describe("реализация функций: map, filter, find, some, every", function() {
     describe("map prototype", function () {
-        console.log("testing works: map prototype");
         it("increment all values in [10,11,12,13,14,15] should return [11,12,13,14,15,16]", () => {
             assert.deepEqual([10, 11, 12, 13, 14, 15].map2((value) => value + 1), [11, 12, 13, 14, 15, 16]);
         });
@@ -14,7 +13,6 @@ describe("реализация функций: map, filter, find, some, every", 
         });
     });
     describe("filter prototype", function () {
-        console.log("testing works: filter prototype");
         it("get all values in [10,11,12,13,14,15] that are %2 should return [10,12,14]", () => {
             assert.deepEqual([10, 11, 12, 13, 14, 15].filter2((value) => value % 2 === 0), [10, 12, 14]);
         });
@@ -23,7 +21,6 @@ describe("реализация функций: map, filter, find, some, every", 
         });
     });
     describe("find prototype", function () {
-        console.log("testing works: find prototype");
         it("find \"Ana\" in [\"John\", \"Alex\", \"Ana\", \"Bob\"] should return \"Ana\"" , () => {
             assert.deepEqual(["John", "Alex", "Ana", "Bob"].find2((value) => value === "Ana"), "Ana");
         });
@@ -34,8 +31,7 @@ describe("реализация функций: map, filter, find, some, every", 
             assert.deepEqual([3, 2, 5, 7, 10, 8].find2((value) => value > 5), 7);
         });
     });
-    describe("some prototype", function () {
-        console.log("testing works: some prototype");
+    describe("some the prototype", function () {
         it("any condition in an empty array should return false" , () => {
             assert.deepEqual([].some2((value) => value > 5), false);
         });
@@ -46,8 +42,7 @@ describe("реализация функций: map, filter, find, some, every", 
             assert.deepEqual(["John", "Alex", "Ana", "Bob"].some2((value) => value === "Agata"), false);
         });
     });
-    describe("every prototype", function () {
-        console.log("testing works: every prototype");
+    describe("every the prototype", function () {
         it("any condition in an empty array should return true" , () => {
             assert.deepEqual([].every2((value) => value > 5), true);
         });
