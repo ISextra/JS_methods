ce3b5052d5ee3edf5c4f45ea5aea8c7168b58a54
ISextra <ivan2001solovei@mail.ru>
2022-08-22 17:40:05 +0300

    stage-1 getMaxWordLengthOfString.js reduce fix

--- src/js/stage-1/getMaxWordLengthOfString.js
+++ src/js/stage-1/getMaxWordLengthOfString.js
@@ -4,11 +4,13 @@ export const getMaxWordLengthOfString = str => {
     let maxlength = 0;
     const str1 = str.split(" ");
 
-    str1.reduce( (previous,current) => {
-        if (current.length > maxlength) {
-            maxlength = current.length;
+    maxlength = str1.reduce( (previous,current) => {
+        if (current.length > previous) {
+            return current.length;
         }
-    });
+
+        return previous;
+    },0);
 
     return maxlength;
 };
