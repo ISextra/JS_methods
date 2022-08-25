02980ecf8a8d8f3d751674c0ae62a1b559ee6d6f
ISextra <ivan2001solovei@mail.ru>
2022-08-25 17:54:06 +0300

    stage-3 searchForMissingSymbol.js add alternative function

--- src/js/stage-3/searchForMissingSymbol.js
+++ src/js/stage-3/searchForMissingSymbol.js
@@ -24,3 +24,28 @@ export const searchForMissingSymbol = (str) => {
 
     return result.join('');
 };
+
+//переделал основной цикл через reduce
+export const searchForMissingSymbolAlt = (str) => {
+    const arrayStr = str.split("");
+    const result = [];
+
+    arrayStr.reduce((prevValue,value) => { //пока не пройдем по всем элементам arrayStr
+        if (value.charCodeAt(0) - prevValue.charCodeAt(0) > 1) { //если разница в кодах текушего и предидущего символа больше 1 (идут не по порядку)
+            let symbolCode = prevValue.charCodeAt(0); //запиминаем код предидущего символа
+
+            while (symbolCode + 1 < value.charCodeAt(0)) { //пока не дойдем до кода текущего символа
+                symbolCode++;//увеличиваем код символа
+                result.push(String.fromCharCode(symbolCode)); //добавляем символ по коду в результирующую строку
+            }
+        }
+
+        return value; //перезаписываем предидущий символ
+    });
+
+    if (result.length < 1) { //если ни одного символа не было добавлено
+        return undefined; //возвращаем undefined
+    }
+
+    return result.join('');
+};
