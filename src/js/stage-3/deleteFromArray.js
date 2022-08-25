b6fa24981d1853f55b597ab4d8450174e9c4ed53
ISextra <ivan2001solovei@mail.ru>
2022-08-25 20:19:43 +0300

    stage-3 deleteFromArray.js fix

--- src/js/stage-3/deleteFromArray.js
+++ src/js/stage-3/deleteFromArray.js
@@ -2,15 +2,5 @@
 // аргументов (точное количество не известно). Удалить из массива все элементы,
 // которые имеют то же значение, что и эти аргументы.
 export const deleteFromArray = (arr,...args) => {
-    let index = 0;
-
-    while (index < arr.length) {
-        if (args.includes(arr[index])) {
-            arr.splice(index,1);
-        } else {
-            index++;
-        }
-    }
-
-    return arr;
-}
+    return arr.filter(value => !args.includes(value));
+};
