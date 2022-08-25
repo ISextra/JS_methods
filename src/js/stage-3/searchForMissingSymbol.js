3d98c17238a6fd386fadef158427c713e3dfd0f5
ISextra <ivan2001solovei@mail.ru>
2022-08-25 23:22:30 +0300

    stage-3 searchForMissingSymbol.js alt fix

--- src/js/stage-3/searchForMissingSymbol.js
+++ src/js/stage-3/searchForMissingSymbol.js
@@ -28,21 +28,23 @@ export const searchForMissingSymbol = (str) => {
 //переделал основной цикл через reduce
 export const searchForMissingSymbolAlt = (str) => {
     const arrayStr = str.split("");
-    const result = [];
 
-    arrayStr.reduce((prevValue,value) => { //пока не пройдем по всем элементам arrayStr
+    const result = arrayStr.reduce((prevValue,value) => { //пока не пройдем по всем элементам arrayStr
         if (value.charCodeAt(0) - prevValue.charCodeAt(0) > 1) { //если разница в кодах текушего и предидущего символа больше 1 (идут не по порядку)
             let symbolCode = prevValue.charCodeAt(0); //запиминаем код предидущего символа
+            let segmentSymbols = '';
 
             while (symbolCode + 1 < value.charCodeAt(0)) { //пока не дойдем до кода текущего символа
                 symbolCode++;//увеличиваем код символа
-                result.push(String.fromCharCode(symbolCode)); //добавляем символ по коду в результирующую строку
+                segmentSymbols = `${segmentSymbols}${String.fromCharCode(symbolCode)}`; //добавляем символ по коду в результирующую строку
             }
+
+            return [...segmentSymbols];
         }
 
         return value; //перезаписываем предидущий символ
     });
-
+console.log(result);
     if (result.length < 1) { //если ни одного символа не было добавлено
         return undefined; //возвращаем undefined
     }
